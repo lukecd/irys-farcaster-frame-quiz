4afbb42472fc20a79d228c806bf79244e6cb73f1
Luke Cassady-Dorion <luke@SPStories.com>
2024-02-08 09:28:11 -0300

    first push

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,29 @@ import {
 	FrameInput,
 } from "frames.js/next/server";
 
+// @ts-ignore
 const reducer = (state, action) => ({ count: state.count + 1 });
-
+// @ts-ignore
 export default async function Home(props) {
 	const previousFrame = getPreviousFrame(props.searchParams);
 
 	return (
+		// @ts-ignore
 		<FrameContainer postUrl="/frames" state={state} previousFrame={previousFrame}>
 			<FrameImage>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-					public:"x2dVQryQru...loCaIEH_bc"
+					public:x2dVQryQru...loCaIEH_bc
 				</div>
-				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">version:"1.0.0"</div>
+				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">version:1.0.0</div>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-					id:"32_3kOMg_e...mxwcLEftwU"
+					id:32_3kOMg_e...mxwcLEftwU
 				</div>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
 					timestamp:1707394669238
 				</div>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">validatorSignatures:</div>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
-					signature:"Q5i693Nl53...0CgmT3c2QA"
+					signature:Q5i693Nl53...0CgmT3c2QA
 				</div>
 				<div tw="w-full h-full bg-slate-700 text-white justify-center items-center">deadlineHeight:1363856</div>
 			</FrameImage>
